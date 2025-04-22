98965dbac07c83a8f8bf2379f9e3ae0487323cda
nimoncaaaa <NIMONCAKOMBATE@GMAIL.COM>
2025-04-22 16:32:12 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Robert Hall\n");
+    printf("2/27/1949\n");
+    printf("work 000-444-0101\n");
+    printf("cell 000-444-0189\n");
+
+    return 0;
+}
